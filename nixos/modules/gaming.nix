b1d665011532d089590a8a817b37189e0a48519a
Thomas Ahn <thomasahn2009@gmail.com>
2026-05-10 11:09:12 -0700

    added gaming

---
+++ nixos/modules/gaming.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: 
+
+{
+  environment.systemPackages = with pkgs; [
+    steam
+  ];
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+  };
+}
